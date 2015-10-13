35e9aaf64e89e63a92272a77bc34b6587fad8266
x4d3 <git@xade.eu>
2015-10-13 13:24:09 +0100

    corrected test

--- spec/controllers/api/v1/accounts/assets_summary_controller_spec.rb
+++ spec/controllers/api/v1/accounts/assets_summary_controller_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Api::V1::Accounts::AssetsSummaryController, :type => :controller 
       expect{get :index, :organization_ids => 'org-fcdf'}.to raise_error(Api::V1::InvalidParameterError)
     end
     it "responds successfully with an HTTP 200 status code" do
-      get :index, :organization_ids => ['org-fcdf']
+      get :index, :organization_ids => [ENVied.GROUP_ID]
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
