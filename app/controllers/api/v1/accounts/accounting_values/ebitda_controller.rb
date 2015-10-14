b83b0c66983fceedabfa3ad3117db7c38bf7505b
x4d3 <git@xade.eu>
2015-10-14 19:17:53 +0100

    implemented ebitda_controller

--- app/controllers/api/v1/accounts/accounting_values/ebitda_controller.rb
+++ app/controllers/api/v1/accounts/accounting_values/ebitda_controller.rb
@@ -1,5 +1,8 @@
 module Api::V1::Accounts::AccountingValues
   # Returns the EBITDA (Revenue - Expenses excluding Interest, Taxes, Depreciation and Amortization) throughout a given period of time.
+  # 
+  # EBITDA = (REVENUE - COSTOFGOODSSOLD - EXPENSE)[where subtype not in EXCLUDED_SUBTYPES] 
+  #
   # Example:
   # {
   #     "organizations": ["org-abcd","org-qwer"],
@@ -18,16 +21,44 @@ module Api::V1::Accounts::AccountingValues
   # }
   class EbitdaController < ApiController
     EBITDA_LEGEND = 'Revenue - Expenses (excluding Taxes, Interests, Depreciation and Amortization)'
+    #Subtype excluded for EBITDA computation
+    EXCLUDED_SUBTYPES = ['GLOBALTAXEXPENSE', 'TAXESPAID']
     def index
-      organization_ids = params['organization_ids']
+      organization_ids = params[:organization_ids]
+      histParameters = getHistParameters
+      dates = []
+      values = []
+      organization_ids.each_with_index  do |organization_id, i|
+        connecAuth = getAuth(organization_id)
+        connecResult = getIncomeStatement(connecAuth, histParameters)
+        income_statements = connecResult['income_statements']
+        #The income_statements contains an extra YTD value at the end that needs to be removed
+        income_statements = income_statements[0...-1]
+        income_statements.each_with_index do |income_statement, j|
+          if (i == 0)
+            dates[j] = income_statement['from']
+            values[j] = 0
+          end
+          revenue = filterAndSumTotal(income_statement['revenue'])
+          cogs = filterAndSumTotal(income_statement['cogs'])
+          expenses = filterAndSumTotal(income_statement['operating_expenses'])
+          values[j] += revenue - cogs - expenses
+        end
+      end
       accounting = {}
+      #FIXME: retrieve currency properly 
       accounting[:currency ] = 'EUR'
-      accounting[:dates ] = 'dates'
+      accounting[:dates ] = dates
       accounting[:legend ] = EBITDA_LEGEND
       accounting[:type ] = 'EBITDA'
-      accounting[:values ] = 'values'
-      result = {:organizations => organization_ids, :accounting => accounting}
-      render json: result
+      accounting[:values ] = values
+      render json: {:organizations => organization_ids, :accounting => accounting, :hist_parameters => histParameters.toHttpQuery}
     end
+    
+    private
+    def filterAndSumTotal(array)
+      array.reject {|x| EXCLUDED_SUBTYPES.include?(x["sub_type"])}.map {|x| x['total']}.inject(:+)
+    end
+    
   end
 end
